950c1db6da444409349a4a169548c8bde25ea815
RJ <radek.jenik36@gmail.com>
2025-02-02 21:56:46 +0100

    chore: clean

--- Round One/Views/MainView.swift
+++ Round One/Views/MainView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct MainView: View {
+    //for later
+    //@Environment(\.modelContext) private var mc
+    //@Query private var items: [Item]
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
