1255d38feb167840a31ec74746266b035db8c73b
RJ <radek.jenik36@gmail.com>
2025-02-02 21:45:36 +0100

    feat: mainview init

---
+++ Round One/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Round One
+//
+//  Created by Radek Jeník on 2/2/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
