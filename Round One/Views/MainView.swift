413ae9826d181f62d185b8cf345bfbfbb24c96e3
RJ <radek.jenik36@gmail.com>
2025-02-02 22:08:21 +0100

    chore: remove unnecessary comments

--- Round One/Views/MainView.swift
+++ Round One/Views/MainView.swift
@@ -7,11 +7,7 @@
 
 import SwiftUI
 
-struct MainView: View {
-    //for later
-    //@Environment(\.modelContext) private var mc
-    //@Query private var items: [Item]
-    
+struct MainView: View {    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
