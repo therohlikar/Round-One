ac2cc4514d52db759a719f7ff05c28d947949d92
RJ <radek.jenik36@gmail.com>
2025-02-04 13:26:21 +0100

    feat: currentuser init

---
+++ Round One/Services/CurrentUser.swift
@@ -0,0 +1,23 @@
+//
+//  CurrentUser.swift
+//  Round One
+//
+//  Created by Radek Jeník on 2/4/25.
+//
+
+class CurrentUser {
+    //will be updated to DB Model of User
+    private var username: String
+    
+    func setUsername(_ username: String) {
+        self.username = username
+    }
+    
+    func getUsername() -> String {
+        return username
+    }
+    
+    init(username: String = "anonym") {
+        self.username = username
+    }
+}
