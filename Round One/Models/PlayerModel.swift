a83cb1846632cad1b832ac6dd8aa8bc39366a376
RJ <radek.jenik36@gmail.com>
2025-02-07 20:27:28 +0100

    feat: playermodel init

---
+++ Round One/Models/PlayerModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlayerModel.swift
+//  Round One
+//
+//  Created by Radek Jeník on 2/7/25.
+//
+
+import Foundation
+
+struct PlayerModel: Hashable {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
