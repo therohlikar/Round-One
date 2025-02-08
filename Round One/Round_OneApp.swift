3378fc11078be9bf5f6f75d7634a03a46e3504cc
RJ <radek.jenik36@gmail.com>
2025-02-08 11:26:29 +0100

    feat: show toasts globally

--- Round One/Round_OneApp.swift
+++ Round One/Round_OneApp.swift
@@ -11,11 +11,25 @@ import SwiftData
 @main
 struct Round_OneApp: App {
     @AppStorage("colorScheme") private var storedColorScheme: String = "dark"
+    @StateObject var toastNotifications: ToastNotificationViewModel = .init()
     
     var body: some Scene {
         WindowGroup {
             MainView()
                 .preferredColorScheme(storedColorScheme == "dark" ? .dark : .light)
+                .environmentObject(toastNotifications)
+                .overlay(alignment: .bottom){
+                    Spacer()
+                    
+                    VStack(spacing: 10){
+                        ForEach(toastNotifications.toasts) { toast in
+                            ToastView(toast: toast)
+                                .transition(.opacity)
+                                .animation(.easeInOut(duration: 0.25), value: toast.id)
+                        }
+                    }
+                    
+                }
         }
     }
 }
