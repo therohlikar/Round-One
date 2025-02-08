030e849bd7fa7337b2db8ff72638c13b993fb8dc
RJ <radek.jenik36@gmail.com>
2025-02-08 11:33:54 +0100

    refactor: update new design

--- Round One/Views/SubViews/ToastView.swift
+++ Round One/Views/SubViews/ToastView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ToastView: View {
+    @Environment(\.colorScheme) var colorScheme
     @State var toast: ToastNotificationModel
     
     var body: some View {
@@ -15,13 +16,18 @@ struct ToastView: View {
             Image(systemName: "exclamationmark.circle")
                 .font(.title3)
                 .foregroundStyle(toast.flagColor)
+            Spacer()
+            
             Text(toast.description)
                 .font(.caption)
+            
+            Spacer()
         }
-        .padding(10)
+        .padding(5)
         .background{
             RoundedRectangle(cornerRadius: 25)
-                .foregroundStyle(.white)
+                .frame(minWidth: 300)
+                .foregroundStyle(toast.flagColor)
                 .opacity(0.3)
         }
         .transition(.move(edge: .top).combined(with: .opacity))
@@ -29,5 +35,5 @@ struct ToastView: View {
 }
 
 #Preview {
-    ToastView(toast: ToastNotificationModel(title: "Title", description: "Lorem ipsum dolor sit amet", flag: .warning))
+    ToastView(toast: ToastNotificationModel(title: "Title", description: "Lorem ipsum dolor sit amet", flag: .low))
 }
