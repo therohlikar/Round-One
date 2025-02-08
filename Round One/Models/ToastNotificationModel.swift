e5fa96fd16b048ca9ecde2be5f75d722adfc58ef
RJ <radek.jenik36@gmail.com>
2025-02-08 11:26:05 +0100

    feat: toastnotificationmodel init

---
+++ Round One/Models/ToastNotificationModel.swift
@@ -0,0 +1,39 @@
+//
+//  ToastNotificationModel.swift
+//  Round One
+//
+//  Created by Radek Jeník on 2/8/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct ToastNotificationModel: Identifiable {
+    enum FlagType: String, CaseIterable {
+        case important
+        case warning
+        case low
+    }
+    
+    var id: UUID = UUID()
+    var title: String
+    var description: String
+    var flag: FlagType
+    
+    var flagColor: Color {
+        switch flag {
+            case .important: .red
+            case .warning: .yellow
+            case .low: .blue
+        }
+        
+    }
+    
+    init(title: String, description: String, flag: FlagType) {
+        self.title = title
+        self.description = description
+        self.flag = flag
+    }
+    
+    
+}
